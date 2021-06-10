f0ea34114175d0b1190665a1d0c0b3bb4ececfc4
Alberto <agonzalez32@hotmail.com>
2021-06-10 14:13:47 -0500

    a
    Signed-off-by: Alberto <agonzalez32@hotmail.com>

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-listado',
-  templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  templateUrl: './listado.component.html'
 })
-export class ListadoComponent implements OnInit {
+export class ListadoComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
+    console.log ('Constructor');
   }
 
 }
