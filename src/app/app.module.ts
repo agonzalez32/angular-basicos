f91158e52ee8f1685380ecacb1bad18199f15de0
Alberto <agonzalez32@hotmail.com>
2021-06-10 13:22:49 -0500

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { contadorComponent } from './contador.component';
+import { contadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    contadorComponent
+    contadorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
