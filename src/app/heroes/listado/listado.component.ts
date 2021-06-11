bc100afc01218b0b870422c5d11ce0ace1c999f7
Alberto <agonzalez32@hotmail.com>
2021-06-11 10:40:37 -0500

    a

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -8,4 +8,12 @@ export class ListadoComponent {
 
   heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Capitan America'];
 
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    this.heroeBorrado = this.heroes.shift() || '';
+    //this.heroes.splice(0, 1);
+    //console.log(this.heroeBorrado);
+  }
+
 }
